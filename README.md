810b52f0db5562d7ff9ad41c36e4ba9fa7a3c9d3
Gabriel Gheorghe <knoppy273@live.com>
2018-10-24 22:24:59 +0300

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LibertyDemos
+##### Description:
 Demo examples for Liberty Engine.
+
+##### Instructions:
+Clone the repo then run bat files.
+You will find the executables in bin/windows../
---
+++ demo001/main.d
@@ -0,0 +1,40 @@
+module main;
+
+import liberty.engine;
+
+import player : Player;
+
+mixin(EngineRun);
+
+/**
+ * Application main.
+ * Create a new scene, then spawn a Player,
+ * then register the scene to the engine.
+**/
+void libertyMain() {
+  new Scene("Scene")
+    .getTree()
+    .spawn!Terrain("DemoTerrain")
+    .build(40.0f, 0.0f, [
+      new Material("res/textures/default.bmp"),
+      new Material("res/textures/blendMap.bmp"),
+      new Material("res/textures/default.bmp"),
+      new Material("res/textures/default.bmp"),
+      new Material("res/textures/default.bmp")
+    ])
+    .getScene()
+    .getTree()
+    .spawn!PointLight("DemoPointLight")
+    .getScene()
+    .getTree()
+    .spawn!PointLight("DemoPointLight2")
+    .setColor(Vector3F(0.0f, 0.0f, 1.0f))
+    .getScene()
+    .getTree()
+    .spawn!Player("Player", false)
+    .getScene()
+    .register();
+
+  CoreEngine.enableVSync();
+  Platform.getWindow().setFullscreen(true);
+}
